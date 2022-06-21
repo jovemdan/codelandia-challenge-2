e77a4581261897dc7c752f757bf863bcfeba7cfe
jovemdan <danilogdev@gmail.com>
2022-06-21 12:45:16 -0300

    feat: add ScrollReveal

---
+++ main.js
@@ -0,0 +1,15 @@
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+  #discount,
+  #nav,
+  #nav .logo,
+  #header,
+  #header .content,
+  #highlights,
+  #highlights .content,
+  #products,
+  #products .card,
+  #footer`);
